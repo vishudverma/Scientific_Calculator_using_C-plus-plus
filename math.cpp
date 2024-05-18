fce89700aea8af65c978987e838b5bff521ee4a9
Vishud Verma <244vishudverma@gmail.com>
2024-05-18 17:19:31 +0530

    the main file and math operations file are added soon to add the input file and other operations

---
+++ math.cpp
@@ -0,0 +1,64 @@
+/*this file contains all the operation logic and was the
+hardest to make cause i had to keep testing the logic again
+and again to fool-proof it just look at it*/
+
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+long double a[], sum=0, diff=0, prod=0, quo=0, log=0;
+
+//operating using array to maqximise output and manage 
+//memory in the best way ppossible will implement linked 
+//lists here  later
+
+long double add(long double x[]= a)
+{
+    int size = sizeof(x)/sizeof(long double);
+    for(int i=0; i < size; ++i)
+    {
+        sum += x[i];
+    }
+
+    return sum;
+    // sum is the sum of n number
+}
+
+long double substract(long double x[]= a)
+{
+    for(int i=0; i < sizeof(x); ++i)
+    {
+        diff += x[i];
+    }
+    return diff;
+    // diff is the difference of n number
+}
+
+long double multiply(long double x[] = a)
+{
+    int size = sizeof(x)/sizeof(long double);
+    for(int i=0; i < size; ++i)
+    {
+        prod *= x[i];
+    }
+    return prod;
+    // prod is the product of n number
+}
+
+long double division(long double x[] = a)
+{
+    int size = sizeof(x)/sizeof(long double);
+    for(int i=0; i < 2; ++i)
+    {
+        quo /= x[i];
+    }
+    return quo;
+    // quo is the quotient
+}
+
+void main()
+{
+    cout << "Math";
+}
