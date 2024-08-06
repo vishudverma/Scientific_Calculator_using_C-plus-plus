bc37dd7558a6cbef5a761d54e4a97420547455b9
vishudverma <244vishudverma@gmail.com>
2024-08-06 14:08:28 +0530

    created a header file to use and included it within main

--- main.cpp
+++ main.cpp
@@ -1,20 +1,8 @@
 #include <iostream>
 #include <cmath>
+#include "math.h"
 
-using namespace std;
-
-double log_base(double x, double base) {
-    double a,b,ans;
-    a = log10(x);
-    b = log10(base);
-    ans = a/b;
-    return ans;
-}
 
 int main() {
-    double x = 8.0;
-    double base = 2.0;
-    double y = log_base(x, base);
-    std::cout << "The logarithm to the base " << base << " of " << x << " is " << y << std::endl;
-    return 0;
-}
+     return 0;
+ }
